7cbb0dc75b844241e58aab96ed2b29db9bc8a833
Albert Mwasisoba <almwassy@gmail.com>
2023-04-05 00:56:36 +0300

    docs: :memo: Utils
    Added skills data

---
+++ components/utils.jsx
@@ -0,0 +1,71 @@
+import * as DiIcons from 'react-icons/di';
+import * as SiIcons from 'react-icons/si';
+
+const skills = [
+    {
+        title: 'Flutter Dev',
+        link: 'https://flutter.dev/',
+        icon: <DiIcons.DiFlutter />,
+    },
+    {
+        title: 'NextJs',
+        link: 'https://nextjs.org/',
+        icon: <SiIcons.SiNextDotJs />,
+    },
+    {
+        title: 'ReactJs',
+        link: 'https://reactjs.org/',
+        icon: <SiIcons.SiReact />,
+    },
+    {
+        title: 'NodeJs',
+        link: 'https://nodejs.org/en/',
+        icon: <SiIcons.SiNodeDotJs />,
+    },
+    {
+        title: 'TailwindCss',
+        link: 'https://tailwindcss.com/',
+        icon: <SiIcons.SiTailwindcss />,
+    },
+    {
+        title: 'Bootstrap',
+        link: 'https://getbootstrap.com/',
+        icon: <SiIcons.SiBootstrap />,
+    },
+    {
+        title: 'mySQL',
+        link: 'https://www.mysql.com/',
+        icon: <SiIcons.SiMysql />,
+    },
+    {
+        title: 'Linux',
+        link: 'https://www.linux.org/',
+        icon: <SiIcons.SiLinux />,
+    },
+    {
+        title: 'Git',
+        link: 'https://git-scm.com/',
+        icon: <SiIcons.SiGit />,
+    },
+    {
+        title: 'Swagger',
+        link: 'https://swagger.io/',
+        icon: <SiIcons.SiSwagger />,
+    },
+    {
+        title: 'Photoshop',
+        link: 'https://www.adobe.com/products/photoshop.html',
+        icon: <SiIcons.SiAdobephotoshop />,
+    },
+    {
+        title: 'Illustrator',
+        link: 'https://www.adobe.com/products/illustrator.html',
+        icon: <SiIcons.SiAdobeillustrator />,
+    },
+]
+
+const data = {
+    skills
+}
+
+export default data
