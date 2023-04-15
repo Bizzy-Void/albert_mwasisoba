4e0a9f5e61ad541b04b5023e9698f181fa7ee5b9
Albert Mwasisoba <almwassy@gmail.com>
2023-04-15 23:03:02 +0300

    style: :art: Added a new font-face and implemented on nav logo

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ function Layout({ children }) {
     <div className='flex relative flex-col h-screen'>
         <nav className='w-full pt-4 pb-2 px-4 md:px-10 lg:px-32 fixed top-0 left-0 z-50 bg-white flex justify-between'>
           <Link href={'/'}>
-            <span className='text-4xl home-link'>
+            <span className='text-4xl font-iskry home-link'>
               <span className='inline md:hidden'>a</span>
               <span className='hidden md:inline lg:hidden'>albert</span>
               <span className='hidden md:hidden lg:inline'>albertmwasisoba</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+        iskry: ["Iskry", "cursive"],
+      }
+    },
   },
   plugins: [],
 }
