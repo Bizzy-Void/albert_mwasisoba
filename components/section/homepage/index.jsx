63db67d294db6aa27be275e0473fe196efe60132
Albert Mwasisoba <almwassy@gmail.com>
2023-04-20 18:26:26 +0300

    feat: :sparkles: Added a hero image

--- components/section/homepage/index.jsx
+++ components/section/homepage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroTexts from './heroText'
+import AlbertImage from './albertImage'
 
 function Homepage() {
   return (
@@ -7,6 +8,7 @@ function Homepage() {
         <div className='relative w-full h-full flex items-center justify-center'>
             <div className='px-4 md:px-10 lg:px-32 absolute top-0 left-0 w-full h-full flex items-center justify-center md:justify-between'>
                 <HeroTexts/>
+                <AlbertImage/>
             </div>
         </div>
     </>
