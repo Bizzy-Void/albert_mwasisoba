b2da37943e66f2d4a3c43554971cb65efb1aa48e
Albert Mwasisoba <almwassy@gmail.com>
2023-06-25 01:40:03 +0300

    feat(UI): :sparkles: Rubberband effect on hero text

---
+++ components/section/homepage/textspan.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react'
+import { motion, useAnimationControls } from 'framer-motion'
+
+function TextSpan({children}) {
+    const controls = useAnimationControls();
+    const [isPlaying, setIsplaying] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+
+    const rubberBand = () => {
+        controls.start({
+            transform: [
+                "scale3d(1,1,1)",
+                "scale3d(1.4,.55,1)",
+                "scale3d(.75,1.25,1)",
+                "scale3d(1.25,.85,1)",
+                "scale3d(.9,1.05,1)",
+                "scale3d(1,1,1)",
+            ],
+
+            transition: {
+                times: [0,.4,.6,.7,.8,.9],
+                ease: "easeInOut",
+            }
+        })
+        setIsplaying(true)
+    }
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
+  return (
+    <motion.span 
+        animate={controls}
+        onMouseOver={() => {
+            if(!isPlaying)
+                rubberBand()
+        }}
+        onAnimationComplete={() => setIsplaying(false)}
+        className={`select-none inline-block font-iskry text-white text-6xl md:text-8xl font-bold ${
+            isHovered ? 'text-gray-700' : 'text-white'
+          }`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+    >
+        {children}
+    </motion.span>
+  )
+}
+
+export default TextSpan
