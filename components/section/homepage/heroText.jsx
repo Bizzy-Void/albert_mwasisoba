48cddcd4386de1e3f9f8c7c741e8c25081555655
Albert Mwasisoba <almwassy@gmail.com>
2023-06-25 01:53:45 +0300

    refactor(UI): Nothing much

--- components/section/homepage/heroText.jsx
+++ components/section/homepage/heroText.jsx
@@ -24,7 +24,7 @@ export default function HeroTexts() {
         })}
         <p className="md:text-xl text-white mb-20 pb-8 capitalize">
             <span className="font-bold">Software </span>
-            <span className="font-bold">developer</span> and{" "}
+            <span className="font-bold">developer</span> &{" "}
             <span className="font-bold">Graphics designer.</span> 
         </p>
     </div>
