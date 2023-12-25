96f8810b4ea55111ca7461350895fe174da58cca
Albert Mwasisoba <almwassy@gmail.com>
2023-12-25 05:02:47 +0300

    refactor: More changes

--- components/section/homepage/heroText.jsx
+++ components/section/homepage/heroText.jsx
@@ -4,13 +4,13 @@ export default function HeroTexts({ onMouseEnter, onMouseLeave, isHoliday, holid
   const firstName = "Albert".split("");
   const lastName = "Mwasisoba".split("");
 
-  const firstText = isHoliday ? "Happy".split("") : "Albert".split("");
-  const lastText = isHoliday ? "Holidays".split("") : "Mwasisoba".split("");
+  const firstText = isHoliday ? "Merry".split("") : "Albert".split("");
+  const lastText = isHoliday ? "Christmas".split("") : "Mwasisoba".split("");
   const colorClass = isHoliday ? holidayColor : "text-gray-500";
 
   return (
     <div className={`font-neue text-center md:text-left z-30 select-none ${colorClass}`}>
-        <h1 className="text-4xl text-gray-500 pb-4">{ isHoliday ? "I wish you" : "Hello, I'm" }</h1>
+        <h1 className="md:text-3xl lg:text-4xl text-gray-500 pb-4">{ isHoliday ? "I wish you" : "Hello, I'm" }</h1>
         {firstText.map((letter, index) => {
           return (
             <TextSpan key={index} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
@@ -29,9 +29,9 @@ export default function HeroTexts({ onMouseEnter, onMouseLeave, isHoliday, holid
         <p className="md:text-xl text-white mb-20 pb-8 capitalize">
             { isHoliday ? (
               <>
-                <span className="font-bold">To you </span>
-                <span className="font-bold">and </span>
-                <span className="font-bold">your family </span>
+                <span className="font-bold">And A </span>
+                <span className="font-bold">Happy </span>
+                <span className="font-bold">New Year </span>
               </>
             ) : (
               <>
