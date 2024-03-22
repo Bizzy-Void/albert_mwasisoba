1468ed2a84a959940038554c14e4e01903b19d15
Albert Mwasisoba <almwassy@gmail.com>
2024-03-22 22:07:41 +0300

    feat(UI): Animated background

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -28,7 +28,12 @@ function Layout({ children }) {
         </nav>
         <main className='w-full flex flex-grow font-neue'>
           <div className='bg-bg_color w-full h-full py-20'>
-            {children}
+            <div className="w-full h-full relative overflow-hidden">
+              <div className="absolute bottom-0 md:top-0 right-4 w-72 h-72 bg-gray-100 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob"></div>
+              <div className="absolute bottom-0 md:top-0 right-20 w-44 h-44 bg-gray-600 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob"></div>
+              <div className="absolute bottom-12 md:bottom-32 right-20 w-72 h-72 bg-gray-400 rounded-full mix-blend-screen filter blur-3xl opacity-20 animate-blob"></div>
+              {children}
+            </div>
           </div>
         </main>
         <Footer data={data.mediaData}/>
