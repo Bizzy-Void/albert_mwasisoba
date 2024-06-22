7ae70ba9887a294ae67b9c0fc8b0ab6aae1b381a
Albert Mwasisoba <almwassy@gmail.com>
2024-06-23 02:19:36 +0300

    refactor(UI): improve navbar
    A new navbar component was created and replaced the one on layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,28 +4,14 @@ import Footer from './section/footer'
 
 import data from './utils'
 
+import Navbar from './ui/navbar'
+
 // custom styles
 
 function Layout({ children }) {
   return (
     <div className='flex relative flex-col h-screen bg-bg_color'>
-        <nav className='w-full pt-4 pb-2 px-4 md:px-10 lg:px-32 fixed top-0 left-0 z-50 bg-black bg-opacity-5 backdrop-filter backdrop-blur-xl p-4 flex justify-between select-none pointer-events-auto'>
-          <Link href={'/'}>
-            <span className='text-3xl font-iskry home-link text-white'>
-              <span className='inline md:hidden'>a</span>
-              <span className='hidden md:inline lg:hidden'>albert</span>
-              <span className='hidden md:hidden lg:inline'>albertmwasisoba</span>
-              <span className='text-gray-500'>.me</span>
-            </span>
-          </Link>
-          <ul className='font-neue text-xs uppercase gap-x-4 flex justify-between'>
-            {data.navRoutes.map((route, index) => (
-              <li key={index}>
-                <NavMenuItem title={route.title} path={route.path} />
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <Navbar/>
         <main className='w-full flex flex-grow font-neue'>
           <div className='bg-bg_color w-full h-full py-20'>
             <div className="w-full h-full relative overflow-hidden">
---
+++ components/ui/navbar/index.tsx
@@ -0,0 +1,15 @@
+import Logo from "./logo"
+import NavItems from "./navItems"
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="w-full pt-4 pb-2 px-4 md:px-10 lg:px-32 fixed top-0 left-0 z-50 bg-black bg-opacity-5 backdrop-filter backdrop-blur-xl p-4 flex justify-between select-none pointer-events-auto">
+                <Logo />
+                <NavItems />
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
